74810cc481600c2e7b9ca553896ba2527108fa6a
fleonis <fleonis@noemail.net>
2019-12-14 15:01:03 +0000

    use cmake's glob, remove unsued files
    FossilOrigin-Name: c68b6efa245cd1d2970ceeed2348fb1ac7e131e8274410c17235ed4118303b5d

--- parser/helpers.cpp
+++
@@ -1,8 +0,0 @@
-/*************************************************************************
- * Copyright © 2019 Hudyaev Alexy <hudyaev.alexy@gmail.com>
- * This file is part of cppjinja.
- * Distributed under the GNU Affero General Public License.
- * See accompanying file copying (at the root of this repository)
- * or <http://www.gnu.org/licenses/> for details
- *************************************************************************/
-
