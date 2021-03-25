07b1cf5c4d8e7519df495f733270e21ee41d1951
fleonis <fleonis@noemail.net>
2021-03-25 16:35:32 +0000

    move simple_data_holder to separate file, add test file (it must be in previus commit)
    FossilOrigin-Name: 6fa1372cd8ec5f495b3f105370ddf5227361d9f62ce608a0c7b71eecdb29b5e2

---
+++ absd/src/simple_data_holder.cpp
@@ -0,0 +1,156 @@
+/*************************************************************************
+ * Copyright © 2021 Hudyaev Alexy <hudyaev.alexy@gmail.com>
+ * This file is part of abstract_data.
+ * Distributed under the GNU Affero General Public License.
+ * See accompanying file copying (at the root of this repository)
+ * or <http://www.gnu.org/licenses/> for details
+ *************************************************************************/
+
+#include "simple_data_holder.hpp"
+
+using abs_data = absd::data;
+using simple_dh = absd::simple_data_holder;
+
+simple_dh::simple_data_holder()
+    : simple_data_holder(std::make_shared<std::pmr::unsynchronized_pool_resource>())
+{
+}
+
+simple_dh::simple_data_holder(std::shared_ptr<std::pmr::memory_resource> mem)
+    : mem(std::move(mem))
+{
+}
+
+std::pmr::memory_resource& simple_dh::storage() const
+{
+	return *mem;
+}
+
+void simple_dh::set_int(std::int64_t v)
+{
+	require_change();
+	pod.emplace(v);
+}
+
+simple_dh& simple_dh::operator = (double v)
+{
+	require_change();
+	pod.emplace(v);
+	return *this;
+}
+
+simple_dh& simple_dh::operator = (bool v)
+{
+	require_change();
+	pod.emplace(v);
+	return *this;
+}
+
+void simple_dh::require_change() const
+{
+	const bool is_setted =
+	    pod.has_value() || !object.empty() || !array.empty();
+	if(is_setted)
+		throw std::runtime_error("double set");
+}
+
+std::pmr::string& simple_dh::str()
+{
+	require_change();
+	std::pmr::string ret(&storage());
+	pod.emplace(std::move(ret));
+	return std::get<std::pmr::string>(*pod);
+}
+
+const std::pmr::string& simple_dh::str() const
+{
+	require_extract_pod();
+	return std::get<std::pmr::string>(*pod);
+}
+
+void simple_dh::require_extract_pod() const
+{
+	if(!pod.has_value())
+		throw std::runtime_error("try to extract not setted value");
+}
+
+simple_dh& simple_dh::put(std::string_view key)
+{
+	require_change();
+	auto ret = std::allocate_shared<simple_dh>(alloc_t(&storage()), mem);
+	object[std::pmr::string(key, &storage())] = ret;
+	return *ret;
+}
+
+simple_dh::self_type simple_dh::by_key(std::string_view key) const
+{
+	require_extract_obj();
+	return object.at(std::pmr::string(key,&storage()));
+}
+
+void simple_dh::require_extract_obj() const
+{
+	if(object.empty())
+		throw std::runtime_error("try to extract object from not setted data");
+}
+
+simple_dh& simple_dh::push_back()
+{
+	require_change();
+	auto ret = std::allocate_shared<simple_dh>(alloc_t(&storage()), mem);
+	array.emplace_back(ret);
+	return *ret;
+}
+
+simple_dh::self_type simple_dh::by_ind(std::int64_t ind) const
+{
+	require_extract_arr();
+	return array.at(ind);
+}
+
+void simple_dh::require_extract_arr() const
+{
+	if(array.empty())
+		throw std::runtime_error("try to extract array from not setted data");
+}
+
+absd::reflection_info simple_dh::reflect() const
+{
+	reflection_info ret{.type=data_type::empty,.size=array.size()};
+	if(!object.empty()) ret.type = data_type::object;
+	else if(!array.empty()) ret.type = data_type::array;
+	else if(std::holds_alternative<std::int64_t>(*pod))
+		ret.type = data_type::integer;
+	else if(std::holds_alternative<double>(*pod))
+		ret.type = data_type::floating_point;
+	else if(std::holds_alternative<bool>(*pod))
+		ret.type = data_type::boolean;
+	else if(std::holds_alternative<std::pmr::string>(*pod))
+		ret.type = data_type::string;
+	for(auto& [k,v]:object) ret.keys.emplace_back(k);
+	return ret;
+}
+
+std::int64_t simple_dh::to_int() const
+{
+	require_extract_pod();
+	return std::get<std::int64_t>(*pod);
+}
+
+double simple_dh::to_double() const
+{
+	require_extract_pod();
+	return std::get<double>(*pod);
+}
+
+std::pmr::string simple_dh::to_string() const
+{
+	require_extract_pod();
+	return std::get<std::pmr::string>(*pod);
+}
+
+bool simple_dh::to_bool() const
+{
+	require_extract_pod();
+	return std::get<bool>(*pod);
+}
---
+++ absd/tests/simple_data_holder_test.cpp
@@ -0,0 +1,93 @@
+/*************************************************************************
+ * Copyright © 2021 Hudyaev Alexy <hudyaev.alexy@gmail.com>
+ * This file is part of abstract_data.
+ * Distributed under the GNU Affero General Public License.
+ * See accompanying file copying (at the root of this repository)
+ * or <http://www.gnu.org/licenses/> for details
+ *************************************************************************/
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE absd_simple_holder
+
+#include <variant>
+#include <boost/test/unit_test.hpp>
+#include <boost/test/data/test_case.hpp>
+#include <boost/test/data/monomorphic.hpp>
+
+#include "mocks.hpp"
+#include "simple_data_holder.hpp"
+
+BOOST_AUTO_TEST_SUITE(absd)
+using absd_mocks::fixture;
+using abs_data = absd::data;
+using simple_dh = absd::simple_data_holder;
+BOOST_AUTO_TEST_SUITE(simple_data_holder_tests)
+void check_reflect(
+        const absd::reflection_info& ri,
+        absd::data_type t,
+        std::size_t is,
+        std::size_t ks)
+{
+	BOOST_TEST(ri.type == t);
+	BOOST_TEST(ri.size == is);
+	BOOST_TEST(ri.keys.size() == ks);
+}
+BOOST_AUTO_TEST_CASE(simple_int)
+{
+	simple_dh hld;
+	check_reflect(hld.reflect(), absd::data_type::empty, 0, 0);
+	hld = 10;
+	check_reflect(hld.reflect(), absd::data_type::integer, 0, 0);
+	BOOST_TEST(hld.to_int() == 10);
+	BOOST_CHECK_THROW(hld = 3.14, std::exception);
+}
+BOOST_AUTO_TEST_CASE(simple_double)
+{
+	simple_dh hld;
+	hld = 2.45;
+	check_reflect(hld.reflect(), absd::data_type::floating_point, 0, 0);
+	BOOST_TEST(hld.to_double() == 2.45);
+	BOOST_CHECK_THROW(hld = false, std::exception);
+}
+BOOST_AUTO_TEST_CASE(simple_bool)
+{
+	simple_dh hld;
+	hld = true;
+	check_reflect(hld.reflect(), absd::data_type::boolean, 0, 0);
+	BOOST_TEST(hld.to_bool() == true);
+	BOOST_CHECK_THROW(hld = false, std::exception);
+	BOOST_CHECK_THROW(hld = 3.14, std::exception);
+}
+BOOST_AUTO_TEST_CASE(simple_string)
+{
+	simple_dh hld;
+	const simple_dh& chld = hld;
+	hld.str() = "kuku";
+	check_reflect(hld.reflect(), absd::data_type::string, 0, 0);
+	BOOST_TEST(hld.to_string() == "kuku");
+	BOOST_CHECK_THROW(hld = false, std::exception);
+	BOOST_TEST(chld.str().get_allocator().resource() == &hld.storage());
+}
+BOOST_AUTO_TEST_CASE(object)
+{
+	simple_dh hld;
+	BOOST_CHECK_THROW(hld.by_key("t1"), std::exception);
+	hld.put("t1") = 42;
+	check_reflect(hld.reflect(), absd::data_type::object, 0, 1);
+	BOOST_CHECK_THROW(hld = false, std::exception);
+	BOOST_TEST(hld.reflect().keys.at(0) == "t1");
+	BOOST_TEST(hld.by_key("t1")->to_int() == 42);
+	BOOST_CHECK_THROW(hld.by_key("no"), std::out_of_range);
+}
+BOOST_AUTO_TEST_CASE(array)
+{
+	simple_dh hld;
+	BOOST_CHECK_THROW(hld.by_ind(0), std::exception);
+	hld.push_back() = 42;
+	check_reflect(hld.reflect(), absd::data_type::array, 1, 0);
+	BOOST_CHECK_THROW(hld = false, std::exception);
+	BOOST_TEST(hld.by_ind(0)->to_int() == 42);
+	BOOST_CHECK_THROW(hld.by_ind(1), std::out_of_range);
+}
+BOOST_AUTO_TEST_SUITE_END() // simple_data_holder_tests
+BOOST_AUTO_TEST_SUITE_END() // absd
