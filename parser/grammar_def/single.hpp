dd248df5c6ad4873c8134860b648ae30ae2df801
fleonis <fleonis@noemail.net>
2021-04-04 18:40:34 +0000

    ops out comment and set procudce errors
    FossilOrigin-Name: fdd2b4092d5708d0c669421ada2138591ba020b15cc946891ff12688cbf0894d

--- parser/grammar_def/single.hpp
+++ parser/grammar_def/single.hpp
@@ -17,12 +17,12 @@
 namespace cppjinja::text {
 
 	auto const filter_call_def =  function_call | var_name;
-	auto const op_out_def = op_term_start >> expr_ops::expr >> op_term_end;
+	auto const op_out_def = op_term_start > expr_ops::expr > op_term_end;
 
 	auto const op_comment_value_def = lexeme[*(char_ >> !comment_term_end) >> char_];
-	auto const op_comment_def = comment_term_start >> !comment_term_end >> op_comment_value >> comment_term_end;
+	auto const op_comment_def = comment_term_start > !comment_term_end > op_comment_value > comment_term_end;
 
-	auto const op_set_def = block_term_start >> lit("set") >> expr_ops::eq_assign >> block_term_end;
+	auto const op_set_def = block_term_start >> lit("set") >> expr_ops::eq_assign > block_term_end;
 
 	auto const filename_def = quoted_string;
 	auto const op_include_def =
